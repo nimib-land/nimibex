357dbec9fb9ceb37d6f9bbb53f62e2cb99d5f9b6
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2026-07-23 20:29:37 +0200

    init nimibook docs

---
+++ docsrc/blocks/chat_bubbles.nim
@@ -0,0 +1,24 @@
+import nimib, nimibook, nimibex / [chat_bubbles]
+
+nbInit(theme = useNimibook)
+
+nbText: "## Chat Bubbles"
+
+let characterLeft = newChatBubbleCharacter(left=true, name="Reader")
+let characterRight = newChatBubbleCharacter(left=false, name="Teacher", borderColor=colSpringGreen)#, image="https://www.svgrepo.com/show/340058/content-delivery-network.svg")
+let characterRight2 = newChatBubbleCharacter(left=false, name="Teacher's assistant", borderColor=colPink)
+
+characterLeft.chat("Hi there!")
+characterRight.chat("Hi! How *are* **you** today?")
+characterRight.chat("Another message")
+characterRight2.chat("I'm also here!")
+characterLeft.chat: hlMd"""
+This is code:
+```py
+def add(a, b):
+  return a + b
+```
+How was _that_?
+"""
+
+nbSave()
--- nimibex.nimble
+++ nimibex.nimble
@@ -14,3 +14,7 @@ requires "nimib >= 0.4.1"
 
 dev:
   requires "nimibook >= 0.4.0"
+
+task buildBook, "Builds the nimiBook docs":
+  selfExec(" r nbook.nim init")
+  selfExec(" r nbook.nim build")
