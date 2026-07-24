b0c390f63f1686cf0b6e1f52577ff9a9b40bb4c0
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2026-07-24 20:09:30 +0200

    blocks index

--- docsrc/blocks/index.nim
+++ docsrc/blocks/index.nim
@@ -1,5 +1,16 @@
-import nimib, nimibook, nimibex / [chat_bubbles]
+import nimib, nimibook
 
 nbInit(theme = useNimibook)
+
+nbText: hlMd"""
+## Blocks
+This section lists and documents all blocks that are available in Nimibex. 
+To import a block you have to explicitly import its submodule.
+We have deliberately chosen to not provide a way to import all modules because
+it would pollute the Nimib renderer and could cause unexpected behavior. This is the correct way to import the chat bubble block:
+"""
+
+nbCode:
+  import nimibex / chat_bubbles
 
 nbSave()
