b5d3f4ebd6019ee92603b4cd9adae8c639fa87d9
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2026-07-24 20:04:11 +0200

    short index

--- docsrc/index.nim
+++ docsrc/index.nim
@@ -2,4 +2,10 @@ import nimib, nimibook, nimibex / [chat_bubbles]
 
 nbInit(theme = useNimibook)
 
+nbText: hlMd"""
+# Nimibex
+
+This repo collects extensions, experiments and extras for the Nimib ecosystem.
+"""
+
 nbSave()
