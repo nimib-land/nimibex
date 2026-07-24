b14be174a61a5073062336ddb8932c319a8e3f94
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2026-07-24 19:40:12 +0200

    start on demo

--- docsrc/blocks/chat_bubbles.nim
+++ docsrc/blocks/chat_bubbles.nim
@@ -4,26 +4,14 @@ nbInit(theme = useNimibook)
 
 nbText: "## Chat Bubbles"
 
+nbText: "### Demo"
+
 let characterLeft = newChatBubbleCharacter(left=true, name="Reader")
 let characterRight = newChatBubbleCharacter(left=false, name="Teacher", borderColor=colSpringGreen)#, image="https://www.svgrepo.com/show/340058/content-delivery-network.svg")
 let characterRight2 = newChatBubbleCharacter(left=false, name="Teacher's assistant", borderColor=colPink)
 
 characterLeft.chat("Hi there!")
-characterRight.chat("Hi! How *are* **you** today?")
-characterRight.chat("Another message")
-characterRight2.chat("I'm also here!")
-characterLeft.chat: hlMd"""
-This is code:
-```py
-def add(a, b):
-  return a + b
-```
-How was _that_?
-"""
+characterLeft.chat("This is a chat bubble!")
 
-characterRight.chat("That is correct!")
-characterRight.chat("Let's try again")
-characterLeft.chat("Hurray! It works!")
-characterLeft.chat("Hope it still works!")
 
 nbSave()
